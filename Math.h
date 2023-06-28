09e3e6ca657345bb2917ef39be3bde5f0921a582
fauder_home <canik.burak@gmail.com>
2023-06-28 17:48:56 +0300

    Math: Add Wrap180() API

--- Math.h
+++ Math.h
@@ -36,6 +36,13 @@ namespace Framework::Math
 	template< std::floating_point Value >
 	Value ToRadians( const Value angle_inDegrees ) { return angle_inDegrees * Constants::DEG_TO_RAD; }
 
+	/* Wraps a given angle (in degrees) between (-180, +180] range (degrees). */
+	template< std::floating_point Value >
+	Value Wrap180( const Value angle_inDegrees )
+	{ 
+		return angle_inDegrees - Value( 360 ) * std::floor( ( angle_inDegrees + Value( 180 ) ) / Value( 360 ) );
+	}
+
 /* Floating point comparisons. */
 	template< std::floating_point Value, auto epsilon = std::numeric_limits< Value >::epsilon() >
 	bool IsEqualTo( Value lhs, Value rhs ) { return std::abs( lhs - rhs ) < epsilon; }
