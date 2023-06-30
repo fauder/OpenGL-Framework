ef27103a9617a585fddf586f22bf7a4f32644234
fauder_home <canik.burak@gmail.com>
2023-07-01 01:41:19 +0300

    🔁 Constants: Include <numbers> and use pi from there to define our PI

--- Constants.h
+++ Constants.h
@@ -1,8 +1,11 @@
 #pragma once
 
+// std Includes.
+#include <numbers>
+
 namespace Framework::Constants
 {
-	constexpr float PI         = 3.14159265359f;
+	constexpr float PI         = std::numbers::pi_v< float >;
 	constexpr float DEG_TO_RAD = PI / 180.0f;
 	constexpr float RAD_TO_DEG = 180.0f / PI;
 }
