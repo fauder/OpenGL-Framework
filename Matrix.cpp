4aa122d814a516480040ce28d036d3622e09cb43
fauder_home <canik.burak@gmail.com>
2023-07-03 16:10:37 +0300

    🔁 Matrix: Make functions suitable constexpr and move them into the header

--- Matrix.cpp
+++ Matrix.cpp
@@ -9,23 +9,6 @@
 namespace Framework::Matrix
 {
 	/* In row-major form. */
-	Matrix4x4 OrthographicProjection( const float left_plane_offset, const float right_plane_offset, const float bottom_plane_offset, const float top_plane_offset, const float near_plane_offset, const float far_plane_offset )
-	{
-		const float f_plus_n  = far_plane_offset + near_plane_offset;
-		const float f_minus_n = far_plane_offset - near_plane_offset;
-
-		return Matrix4x4
-		(
-			{
-				1.0f / right_plane_offset,			0.0f,								0.0f,						0.0f,
-				0.0f,								1.0f / top_plane_offset,			0.0f,						0.0f,
-				0.0f,								0.0f,								-2.0f / f_minus_n,			0.0f,
-				0.0f,								0.0f,								-f_plus_n / f_minus_n,		1.0f
-			}
-		);
-	}
-
-	/* In row-major form. */
 	Matrix4x4 PerspectiveProjection( const float near_plane_offset, const float far_plane_offset, const float aspect_ratio, const float vertical_fieldOfView_inDegrees )
 	{
 		const auto half_fov_inRadians = Math::ToRadians( vertical_fieldOfView_inDegrees / 2.0f );
@@ -45,71 +28,6 @@ namespace Framework::Matrix
 				0.0f,								near_plane_offset / half_height,	0.0f,										0.0f,
 				0.0f,								0.0f,								-f_plus_n / f_minus_n,						-1.0f,
 				0.0f,								0.0f,								-2.0f * far_plane_offset / f_minus_n,		0.0f
-			}
-		);
-	}
-
-	Matrix4x4 Scaling( const float newScale )
-	{
-		return Matrix4x4
-		(
-			{
-				newScale,	0.0f,		0.0f,		0.0f,
-				0.0f,		newScale,	0.0f,		0.0f,
-				0.0f,		0.0f,		newScale,	0.0f,
-				0.0f,		0.0f,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 Scaling( const float newScale_x, const float newScale_y, const float newScale_z )
-	{
-		return Matrix4x4
-		(
-			{
-				newScale_x,	0.0f,		0.0f,		0.0f,
-				0.0f,		newScale_y,	0.0f,		0.0f,
-				0.0f,		0.0f,		newScale_z,	0.0f,
-				0.0f,		0.0f,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 ScalingOnX( const float newScale )
-	{
-		return Matrix4x4
-		(
-			{
-				newScale,	0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				0.0f,		0.0f,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 ScalingOnY( const float newScale )
-	{
-		return Matrix4x4
-		( 
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		newScale,	0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				0.0f,		0.0f,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 ScalingOnZ( const float newScale )
-	{
-		return Matrix4x4
-		(
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		newScale,	0.0f,
-				0.0f,		0.0f,		0.0f,		1.0f
 			}
 		);
 	}
@@ -219,58 +137,6 @@ namespace Framework::Matrix
 				nx * ny_times_one_minus_cosTheta - nz_sinTheta,		ny * ny_times_one_minus_cosTheta + cosTheta,			nz * ny_times_one_minus_cosTheta + nx_sinTheta,		0.0f,
 				nx * nz_times_one_minus_cosTheta + ny_sinTheta,		ny * nz_times_one_minus_cosTheta - nx_sinTheta,			nz * nz_times_one_minus_cosTheta + cosTheta,		0.0f,
 				0.0f,												0.0f,													0.0f,												1.0f
-			}
-		);
-	}
-
-	Matrix4x4 Translation( const float delta_x, const float delta_y, const float delta_z )
-	{
-		return Matrix4x4
-		(
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				delta_x,	delta_y,	delta_z,	1.0f
-			}
-		);
-	}
-
-	Matrix4x4 TranslationOnX( const float delta )
-	{
-		return Matrix4x4
-		(
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				delta,		0.0f,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 TranslationOnY( const float delta )
-	{
-		return Matrix4x4
-		(
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				0.0f,		delta,		0.0f,		1.0f
-			}
-		);
-	}
-
-	Matrix4x4 TranslationOnZ( const float delta )
-	{
-		return Matrix4x4
-		(
-			{
-				1.0f,		0.0f,		0.0f,		0.0f,
-				0.0f,		1.0f,		0.0f,		0.0f,
-				0.0f,		0.0f,		1.0f,		0.0f,
-				0.0f,		0.0f,		delta,		1.0f
 			}
 		);
 	}
--- Matrix.h
+++ Matrix.h
@@ -4,19 +4,95 @@
 #include "MatrixBase.hpp"
 #include "Vector.hpp"
 
+/* Constexpr functions can not be in the implementation file. So the ones that are constexpr are on this header file, the ones not constexpr are on the impl. (cpp) file. */
 namespace Framework::Matrix
 {
-	/* In row-major form. */ Matrix4x4 OrthographicProjection( const float left_plane_offset,   const float right_plane_offset,
-															   const float bottom_plane_offset, const float top_plane_offset,
-															   const float near_plane_offset,   const float far_plane_offset );
+
+	/* In row-major form. */
+	constexpr Matrix4x4 OrthographicProjection( const float left_plane_offset, const float right_plane_offset, const float bottom_plane_offset, const float top_plane_offset, 
+									  const float near_plane_offset, const float far_plane_offset )
+	{
+		const float f_plus_n = far_plane_offset + near_plane_offset;
+		const float f_minus_n = far_plane_offset - near_plane_offset;
+
+		return Matrix4x4
+		(
+			{
+				1.0f / right_plane_offset,			0.0f,								0.0f,						0.0f,
+				0.0f,								1.0f / top_plane_offset,			0.0f,						0.0f,
+				0.0f,								0.0f,								-2.0f / f_minus_n,			0.0f,
+				0.0f,								0.0f,								-f_plus_n / f_minus_n,		1.0f
+			}
+		);
+	}
+
 	/* In row-major form. */ Matrix4x4 PerspectiveProjection( const float near_plane_offset, const float far_plane_offset,
 															  const float aspect_ratio,      const float field_of_view_inDegrees );
 
-	/* In row-major form. */ Matrix4x4 Scaling( const float newScale );
-	/* In row-major form. */ Matrix4x4 Scaling( const float newScale_x, const float newScale_y, const float newScale_z );
-	/* In row-major form. */ Matrix4x4 ScalingOnX( const float newScale );
-	/* In row-major form. */ Matrix4x4 ScalingOnY( const float newScale );
-	/* In row-major form. */ Matrix4x4 ScalingOnZ( const float newScale );
+	/* In row-major form. */ constexpr Matrix4x4 Scaling( const float newScale )
+	{
+		return Matrix4x4
+		(
+			{
+				newScale,	0.0f,		0.0f,		0.0f,
+				0.0f,		newScale,	0.0f,		0.0f,
+				0.0f,		0.0f,		newScale,	0.0f,
+				0.0f,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 Scaling( const float newScale_x, const float newScale_y, const float newScale_z )
+	{
+		return Matrix4x4
+		(
+			{
+				newScale_x,	0.0f,		0.0f,		0.0f,
+				0.0f,		newScale_y,	0.0f,		0.0f,
+				0.0f,		0.0f,		newScale_z,	0.0f,
+				0.0f,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 ScalingOnX( const float newScale )
+	{
+		return Matrix4x4
+		(
+			{
+				newScale,	0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				0.0f,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 ScalingOnY( const float newScale )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		newScale,	0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				0.0f,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 ScalingOnZ( const float newScale )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		newScale,	0.0f,
+				0.0f,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
 
 	/* In row-major form. */ Matrix4x4 GeneralRotation( const Vector3& angles_xyz_inDegrees );
 	/* In row-major form. */ Matrix4x4 RotationAroundX( const float angle_inDegrees );
@@ -24,8 +100,55 @@ namespace Framework::Matrix
 	/* In row-major form. */ Matrix4x4 RotationAroundZ( const float angle_inDegrees );
 	/* In row-major form. */ Matrix4x4 RotationAroundAxis( const float angle_inDegrees, Vector3 vector );
 
-	/* In row-major form. */ Matrix4x4 TranslationOnX( const float delta );
-	/* In row-major form. */ Matrix4x4 TranslationOnY( const float delta );
-	/* In row-major form. */ Matrix4x4 TranslationOnZ( const float delta );
-	/* In row-major form. */ Matrix4x4 Translation( const float delta_x, const float delta_y, const float delta_z );
+	/* In row-major form. */ constexpr Matrix4x4 TranslationOnX( const float delta )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				delta,		0.0f,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 Translation( const float delta_x, const float delta_y, const float delta_z )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				delta_x,	delta_y,	delta_z,	1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 TranslationOnY( const float delta )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				0.0f,		delta,		0.0f,		1.0f
+			}
+		);
+	}
+
+	/* In row-major form. */ constexpr Matrix4x4 TranslationOnZ( const float delta )
+	{
+		return Matrix4x4
+		(
+			{
+				1.0f,		0.0f,		0.0f,		0.0f,
+				0.0f,		1.0f,		0.0f,		0.0f,
+				0.0f,		0.0f,		1.0f,		0.0f,
+				0.0f,		0.0f,		delta,		1.0f
+			}
+		);
+	}
 };
