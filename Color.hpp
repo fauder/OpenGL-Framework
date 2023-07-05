386585b4d7041efcd0c56c39dce517d20bfa7da4
fauder_home <canik.burak@gmail.com>
2023-07-05 17:28:15 +0300

    🔁 Color: Rename Color to Color4 and create Color3 as well + constexpr accessors

--- Color.hpp
+++ Color.hpp
@@ -1,26 +1,49 @@
 #pragma once
 
 // Project Includes.
+#include "Concepts.h"
 #include "Vector.hpp"
 
 namespace Framework
 {
-	class Color : public VectorBase< float, 4 >
+	class Color3 : public VectorBase< float, 3 >
 	{
+		using Base = VectorBase< float, 3 >;
+
 	public:
-		Color()
+		Color3()
 			:
-			VectorBase< float, 4 >()
+			Base()
 		{}
 
-		Color( const float r, const float g, const float b, const float a = 1.0f )
+		Color3( const float r, const float g, const float b )
 			:
-			VectorBase< float, 4 >( r, g, b, a )
+			Base( r, g, b )
 		{}
 
-		inline float R() const { return data[ 0 ]; };
-		inline float G() const { return data[ 1 ]; };
-		inline float B() const { return data[ 2 ]; };
-		inline float A() const { return data[ 3 ]; };
+		constexpr float R() const { return data[ 0 ]; };
+		constexpr float G() const { return data[ 1 ]; };
+		constexpr float B() const { return data[ 2 ]; };
+	};
+
+	class Color4 : public VectorBase< float, 4 >
+	{
+		using Base = VectorBase< float, 4 >;
+	
+	public:
+		Color4()
+			:
+			Base()
+		{}
+
+		Color4( const float r, const float g, const float b, const float a = 1.0f )
+			:
+			Base( r, g, b, a )
+		{}
+
+		constexpr float R() const { return data[ 0 ]; };
+		constexpr float G() const { return data[ 1 ]; };
+		constexpr float B() const { return data[ 2 ]; };
+		constexpr float A() const { return data[ 3 ]; };
 	};
 }
