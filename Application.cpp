d81fab1d04ac39493586688c2464661f09e3f0b8
fauder_home <canik.burak@gmail.com>
2023-07-06 02:56:21 +0300

    🔁 Application: Utilize _deg literal instead of explicitly constructing Degrees objects

--- Application.cpp
+++ Application.cpp
@@ -20,6 +20,7 @@
 int main()
 {
 	using namespace Framework;
+	using namespace Math::Literals;
 
 	GLFWwindow* window = nullptr;
 	
@@ -105,7 +106,7 @@ int main()
 		/* Putting the "camera" on z +3 means moving the world to z -3.*/
 		const auto view = Matrix::Translation( 0.0f, 0.0f, -3.0f );
 
-		const auto projection = Matrix::PerspectiveProjection( 0.1f, 100.0f, aspect_ratio, Degrees( 45.0f ) );
+		const auto projection = Matrix::PerspectiveProjection( 0.1f, 100.0f, aspect_ratio, 45.0_deg );
 
 		shader.SetMatrix( "transformation_view",		view );
 		shader.SetMatrix( "transformation_projection",	projection );
@@ -119,7 +120,7 @@ int main()
 			shader.Bind();
 
 			const float time = static_cast< float >( glfwGetTime() );
-			cube_transform.SetRotation( Degrees( time * 50.0f ), Vector3( 0.5f, 1.0f, 0.0f ) );
+			cube_transform.SetRotation( time * 50.0_deg, Vector3( 0.5f, 1.0f, 0.0f ) );
 			shader.SetMatrix( "transformation_world", cube_transform.GetFinalMatrix() );
 
 			renderer.Update( window );
