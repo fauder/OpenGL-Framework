b9fb865f6297eb97fdead9440f03b8aecf213837
fauder_home <canik.burak@gmail.com>
2023-07-10 19:38:53 +0300

    Constants: Add Epsilon< T >() & TwoEpsilon< T >() functions

--- Constants.h
+++ Constants.h
@@ -9,4 +9,16 @@ namespace Framework::Constants
 	constexpr float TWO_PI     = PI * 2.0f;
 	constexpr float DEG_TO_RAD = PI / 180.0f;
 	constexpr float RAD_TO_DEG = 180.0f / PI;
+
+	template< std::floating_point T >
+	constexpr T Epsilon()
+	{
+		if constexpr( std::is_same_v< T, float > )
+			return 10e-5f;
+		else
+			return 10e-15;
+	}
+
+	template< std::floating_point T >
+	constexpr T TwoEpsilon() { return 2 * Epsilon< T >(); }
 }
