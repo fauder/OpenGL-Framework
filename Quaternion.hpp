dac9a3e0abede340689fd231403da24e91d05349
fauder <canik.burak@gmail.com>
2023-07-19 19:37:46 +0300

    🔁 QuaternionBase: Rename to Quaternion & move under Framework::Math namespace

--- Quaternion.hpp
+++ Quaternion.hpp
@@ -10,17 +10,17 @@
 #include "Assert.h"
 #endif // _DEBUG
 
-namespace Framework
+namespace Framework::Math
 {
 /* Forward Declarations. */
 	template< std::floating_point ComponentType >
-	class QuaternionBase;
+	class Quaternion;
 
 	template< std::floating_point ComponentType >
-	constexpr ComponentType Dot( const QuaternionBase< ComponentType >& q1, const QuaternionBase< ComponentType >& q2 );
+	constexpr ComponentType Dot( const Quaternion< ComponentType >& q1, const Quaternion< ComponentType >& q2 );
 
 	template< std::floating_point ComponentType >
-	class QuaternionBase
+	class Quaternion
 	{
 	private:
 		using VectorType  = Math::Vector< ComponentType, 3 >;
@@ -29,33 +29,33 @@ namespace Framework
 	public:
 	/* Constructors. */
 		/* Identity quaternion w=1 & n=<0,0,0>. */
-		constexpr QuaternionBase()
+		constexpr Quaternion()
 			:
 			xyz(),
 			w( 1 )
 		{}
 
-		constexpr QuaternionBase( const QuaternionBase& other )					= default;
-		constexpr QuaternionBase( QuaternionBase && donor )						= default;
-		constexpr QuaternionBase& operator = ( const QuaternionBase & other )	= default;
-		constexpr QuaternionBase& operator = ( QuaternionBase && donor )		= default;
+		constexpr Quaternion( const Quaternion& other )					= default;
+		constexpr Quaternion( Quaternion && donor )						= default;
+		constexpr Quaternion& operator = ( const Quaternion & other )	= default;
+		constexpr Quaternion& operator = ( Quaternion && donor )		= default;
 
-		constexpr ~QuaternionBase() = default;
+		constexpr ~Quaternion() = default;
 
-		constexpr QuaternionBase( const ComponentType x, const ComponentType y, const ComponentType z, const ComponentType w )
+		constexpr Quaternion( const ComponentType x, const ComponentType y, const ComponentType z, const ComponentType w )
 			:
 			xyz( x, y, z ),
 			w( w )
 		{}
 
-		constexpr QuaternionBase( const VectorType& xyz, const ComponentType w )
+		constexpr Quaternion( const VectorType& xyz, const ComponentType w )
 			:
 			xyz( xyz ),
 			w( w )
 		{}
 
 		/* Expects a unit vector for the axis! */
-		QuaternionBase( RadiansType angle, const VectorType& rotation_axis_normalized )
+		Quaternion( RadiansType angle, const VectorType& rotation_axis_normalized )
 		{
 			const RadiansType half_angle = angle / 2.0f;
 			w   = std::cos( ComponentType( half_angle ) );
@@ -77,14 +77,14 @@ namespace Framework
 		}
 
 		// Returns half the angular displacement between *this Quaternion and the other.
-		constexpr RadiansType HalfAngleBetween( const QuaternionBase& other ) const
+		constexpr RadiansType HalfAngleBetween( const Quaternion& other ) const
 		{
 		#ifdef _DEBUG
 			ASSERT( this->IsNormalized() && R"(Quaternion::HalfAngleBetween(other) : The quaternion "*this" is not normalized!)" );
 			ASSERT( other.IsNormalized() && R"(Quaternion::HalfAngleBetween(other) : The quaternion "other" is not normalized!)" );
 		#endif
 
-			return RadiansType( std::acos( Framework::Dot( *this, other ) ) );
+			return RadiansType( std::acos( Framework::Math::Dot( *this, other ) ) );
 		}
 
 		constexpr RadiansType Angle() const
@@ -93,7 +93,7 @@ namespace Framework
 		}
 
 		// Returns the angular displacement between *this Quaternion and the other.
-		constexpr RadiansType AngleBetween( const QuaternionBase& other ) const
+		constexpr RadiansType AngleBetween( const Quaternion& other ) const
 		{
 			return ComponentType{ 2 } * HalfAngleBetween( other );
 		}
@@ -122,12 +122,12 @@ namespace Framework
 		}
 
 	/* Arithmetic Operations: Binary operators (with a scalar), of the the form vector-operator-scalar. */
-		constexpr QuaternionBase operator* ( const ComponentType scalar ) const
+		constexpr Quaternion operator* ( const ComponentType scalar ) const
 		{
 			return { xyz * scalar, w * scalar };
 		}
 
-		constexpr QuaternionBase& operator*= ( const ComponentType scalar )
+		constexpr Quaternion& operator*= ( const ComponentType scalar )
 		{
 			w   *= scalar;
 			xyz *= scalar;
@@ -135,13 +135,13 @@ namespace Framework
 			return *this;
 		}
 
-		constexpr QuaternionBase operator/ ( const ComponentType scalar ) const
+		constexpr Quaternion operator/ ( const ComponentType scalar ) const
 		{
 			const auto inverse_of_scalar = ComponentType( 1 ) / scalar; // We can calculate the inverse safely.
 			return { xyz * inverse_of_scalar, w * inverse_of_scalar };
 		}
 
-		constexpr QuaternionBase& operator/= ( const ComponentType scalar )
+		constexpr Quaternion& operator/= ( const ComponentType scalar )
 		{
 			const auto inverse_of_scalar = ComponentType( 1 ) / scalar; // We can calculate the inverse safely.
 			w   *= inverse_of_scalar;
@@ -151,33 +151,33 @@ namespace Framework
 		}
 
 	/* Other Arithmetic Operations. */
-		constexpr QuaternionBase operator*( const QuaternionBase& other ) const
+		constexpr Quaternion operator*( const Quaternion& other ) const
 		{
-			return QuaternionBase( w * other.xyz + other.w * xyz + Framework::Math::Cross( xyz, other.xyz ),
+			return Quaternion( w * other.xyz + other.w * xyz + Framework::Math::Cross( xyz, other.xyz ),
 								   w * other.w - Framework::Math::Dot( xyz, other.xyz ) );
 		}
 
-		constexpr QuaternionBase Conjugate() const
+		constexpr Quaternion Conjugate() const
 		{
-			return QuaternionBase( -xyz, w );
+			return Quaternion( -xyz, w );
 		}
 
 		/* Sets this quaternion equal to its conjugate (i.e, [-x -y -z w]). */
-		constexpr QuaternionBase& SetToConjugate()
+		constexpr Quaternion& SetToConjugate()
 		{
 			xyz = -xyz;
 
 			return *this;
 		}
 
-		constexpr QuaternionBase Inverse() const
+		constexpr Quaternion Inverse() const
 		{
 			return Conjugate() / Dot();
 		}
 
 		/* Assumes *this is a unit quaternion. Faster than Inverse().
 		 * Simply returns the conjugate since inverse = conjugate / magnitude, where magnitude = 1 for a unit quaternion. */
-		constexpr QuaternionBase Inverse_Normalized() const
+		constexpr Quaternion Inverse_Normalized() const
 		{
 		#ifdef _DEBUG
 			ASSERT( IsNormalized() && R"(Quaternion::Inverse_Normalized() : The quaternion "*this" is not normalized!)" );
@@ -186,13 +186,13 @@ namespace Framework
 			return Conjugate();
 		}
 
-		constexpr QuaternionBase& Invert()
+		constexpr Quaternion& Invert()
 		{
 			return *this = Inverse();
 		}
 
 		/* Sets this quaternion equal to its inverse, which is its conjugate since a unit quaternion is assumed. (i.e, [w -x -y -z]). */
-		constexpr QuaternionBase& Invert_Normalized()
+		constexpr Quaternion& Invert_Normalized()
 		{
 			return SetToConjugate();
 		}
@@ -200,15 +200,15 @@ namespace Framework
 		/* Assumes unit quaternions.
 		 * Returns a quaternion d such that d*a=b (where a = *this Quaternion).
 		 * Here, d is the angular displacement between *this and the other. */
-		constexpr QuaternionBase DifferenceBetween( const QuaternionBase& b ) const
+		constexpr Quaternion DifferenceBetween( const Quaternion& b ) const
 		{
 			return b * Inverse();
 		}
 
 		/* Taken from https://gamemath.com/book/orient.html#quaternions. */
-		constexpr QuaternionBase Exp( const ComponentType exponent ) const
+		constexpr Quaternion Exp( const ComponentType exponent ) const
 		{
-			QuaternionBase result( *this );
+			Quaternion result( *this );
 
 			// Check for the case of an identity quaternion. This will protect against divide by zero.
 			if( std::abs( result.w ) < ComponentType{ 0.9999 } )
@@ -225,7 +225,7 @@ namespace Framework
 			return result;
 		}
 
-		constexpr QuaternionBase SlerpNaive( const QuaternionBase& other, const ComponentType t ) const
+		constexpr Quaternion SlerpNaive( const Quaternion& other, const ComponentType t ) const
 		{
 			return DifferenceBetween( other ).Exp( t ) * *this;
 		}
@@ -240,7 +240,7 @@ namespace Framework
 		}
 
 		template< std::floating_point ComponentType_ > // Have to use a different template parameter here because C++...
-		constexpr friend ComponentType_ Dot( const QuaternionBase< ComponentType_ >& q1, const QuaternionBase< ComponentType_ >& q2 );
+		constexpr friend ComponentType_ Dot( const Quaternion< ComponentType_ >& q1, const Quaternion< ComponentType_ >& q2 );
 
 	private:
 		VectorType xyz;
@@ -248,11 +248,14 @@ namespace Framework
 	};
 
 	template< std::floating_point ComponentType >
-	constexpr ComponentType Dot( const QuaternionBase< ComponentType >& q1, const QuaternionBase< ComponentType >& q2 )
+	constexpr ComponentType Dot( const Quaternion< ComponentType >& q1, const Quaternion< ComponentType >& q2 )
 	{
 		return q1.w * q2.w + Dot( q1.xyz, q2.xyz );
 	}
+}
 
-	using Quaternion  = QuaternionBase< float  >;
-	using QuaternionD = QuaternionBase< double >;
+namespace Framework
+{
+	using Quaternion  = Math::Quaternion< float  >;
+	using QuaternionD = Math::Quaternion< double >;
 }
