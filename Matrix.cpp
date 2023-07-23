525fadc7b1f29dc7c6cb46816e43dc85533e9c9c
fauder <canik.burak@gmail.com>
2023-07-24 01:42:42 +0300

    Matrix: Implement in-place modifications of GeneralRotation() & RotationAroundAxis()

--- Matrix.cpp
+++ Matrix.cpp
@@ -59,6 +59,29 @@ namespace Framework::Matrix
 		);
 	}
 
+	/* In-place modification of the upper-left 3x3 portion. */
+	void Matrix::GeneralRotation( Matrix4x4& matrix, Degrees around_x, Degrees around_y, Degrees around_z )
+	{
+		const Radians alpha( around_x );
+		const Radians  beta( around_y );
+		const Radians gamma( around_z );
+
+		const auto sin_alpha = std::sin( float( alpha ) );
+		const auto sin_beta  = std::sin( float( beta  ) );
+		const auto sin_gamma = std::sin( float( gamma ) );
+
+		const auto cos_alpha = std::cos( float( alpha ) );
+		const auto cos_beta  = std::cos( float( beta  ) );
+		const auto cos_gamma = std::cos( float( gamma ) );
+
+		const auto sin_beta_cos_gamma = sin_beta * cos_gamma;
+		const auto sin_beta_sin_gamma = sin_beta * sin_gamma;
+
+		matrix[ 0 ][ 0 ] = cos_beta * cos_gamma;									matrix[ 0 ][ 1 ] = cos_beta * sin_gamma;									matrix[ 0 ][ 2 ] = -sin_beta;
+		matrix[ 1 ][ 0 ] = sin_alpha * sin_beta_cos_gamma - cos_alpha * sin_gamma;	matrix[ 1 ][ 1 ] = sin_alpha * sin_beta_sin_gamma + cos_alpha * cos_gamma;	matrix[ 1 ][ 2 ] = sin_alpha * cos_beta;
+		matrix[ 2 ][ 0 ] = cos_alpha * sin_beta_cos_gamma + sin_alpha * sin_gamma,	matrix[ 2 ][ 1 ] = cos_alpha * sin_beta_sin_gamma - sin_alpha * cos_gamma;	matrix[ 2 ][ 2 ] = cos_alpha * cos_beta;
+	}
+
 	Matrix4x4 RotationAroundX( Degrees angle )
 	{
 		const Radians alpha( angle );
@@ -138,5 +161,30 @@ namespace Framework::Matrix
 				0.0f,												0.0f,													0.0f,												1.0f
 			}
 		);
+	}
+
+	/* In-place modification of the upper-left 3x3 portion. */
+	void Matrix::RotationAroundAxis( Matrix4x4& matrix, Degrees angle, Vector3 vector )
+	{
+		vector.Normalize();
+
+		const Radians theta( angle );
+
+		const auto nx                           = vector.X();
+		const auto ny                           = vector.Y();
+		const auto nz                           = vector.Z();
+		const auto cos_theta                    = std::cos( float( theta ) );
+		const auto sin_theta                    = std::sin( float( theta ) );
+		const auto one_minus_cos_theta          = 1.0f - cos_theta;
+		const auto nx_times_one_minus_cos_theta = nx * one_minus_cos_theta;
+		const auto ny_times_one_minus_cos_theta = ny * one_minus_cos_theta;
+		const auto nz_times_one_minus_cos_theta = nz * one_minus_cos_theta;
+		const auto nx_sin_theta                 = nx * sin_theta;
+		const auto ny_sin_theta                 = ny * sin_theta;
+		const auto nz_sin_theta                 = nz * sin_theta;
+
+		matrix[ 0 ][ 0 ] = nx * nx_times_one_minus_cos_theta + cos_theta;		matrix[ 0 ][ 1 ] = ny * nx_times_one_minus_cos_theta + nz_sin_theta;	matrix[ 0 ][ 2 ] = nz * nx_times_one_minus_cos_theta - ny_sin_theta;
+		matrix[ 1 ][ 0 ] = nx * ny_times_one_minus_cos_theta - nz_sin_theta;	matrix[ 1 ][ 1 ] = ny * ny_times_one_minus_cos_theta + cos_theta;		matrix[ 1 ][ 2 ] = nz * ny_times_one_minus_cos_theta + nx_sin_theta;
+		matrix[ 2 ][ 0 ] = nx * nz_times_one_minus_cos_theta + ny_sin_theta;	matrix[ 2 ][ 1 ] = ny * nz_times_one_minus_cos_theta - nx_sin_theta;	matrix[ 2 ][ 2 ] = nz * nz_times_one_minus_cos_theta + cos_theta;	
 	}
 }
