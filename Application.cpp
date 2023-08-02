dd4b954372b8be66dd819b884d7afb1f491bbe8e
fauder <canik.burak@gmail.com>
2023-08-02 19:20:43 +0300

    Test: Add new test for ImGui

--- Application.cpp
+++ Application.cpp
@@ -15,9 +15,9 @@
 
 #include "Test/Test_ClearColor.h"
 #include "Test/Test_Transform_2Cubes.h"
+#include "Test/Test_ImGui.h"
 
 // std Includes.
-#include <istream>
 #include <vector>
 #include <map>
 
@@ -25,12 +25,15 @@ int main()
 {
 	using namespace Framework::Test;
 
+	// TODO: Switch to keeping lambdas inside the map, so the Tests are not "built" (e.q., constructed) initially.
+
 	std::map< std::string, std::unique_ptr< TestInterface > > tests_by_name;
 
-	//tests_by_name[ "clear_color" ] = std::make_unique< Test_ClearColor >();
-	tests_by_name[ "transform_2_cubes" ] = std::make_unique< Test_Transfom_2Cubes >();
+	//tests_by_name[ "clear_color" ] = std::make_unique< Test_ClearColor >( nullptr, Framework::Color4::Yellow() );
+	//tests_by_name[ "transform_2_cubes" ] = std::make_unique< Test_Transfom_2Cubes >();
+	tests_by_name[ "imgui" ] = std::make_unique< Test_ImGui >();
 
-	const auto& current_test = tests_by_name[ "transform_2_cubes" ];
+	const auto& current_test = tests_by_name[ "imgui" ];
 
 	current_test->Run();
 
